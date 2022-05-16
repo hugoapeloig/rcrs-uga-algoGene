082be0b58dffaf3d6694c779e9e5bd19de25f334
TheoJ5 <91073471+TheoJ5@users.noreply.github.com>
2022-05-16 07:51:24 +0200

    Create Convertisseur.java

---
+++ src/main/java/Convertisseur.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+import convertisseur_map_to_graph.maps.gml.GMLBuilding;
+import convertisseur_map_to_graph.maps.gml.GMLMap;
+import convertisseur_map_to_graph.maps.gml.GMLRoad;
+import graphResolution.Graph;
+
+
+public class Convertisseur {
+	
+	
+	GMLMap map;
+	Graph graph;
+	
+	
+	public ConvertisseurV5(GMLMap map){
+		
+		this.map
+		
+	}
+
+	
+}
